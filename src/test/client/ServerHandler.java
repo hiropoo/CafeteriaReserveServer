145c37481d52e5cbf5188d945a01b0f7f456b1c5
Hiroshi Kawarabata <125180473+hiropoo@users.noreply.github.com>
2024-06-22 12:28:55 +0900

    サーバとクライアント間で行う処理の枠組みを設定しました (#2)
    * 🚧 ClientがServerと行う処理に関するメソッドを書き出し
    
    * 🚧 Serverがクライアントからのリクエストに応じて行う処理のメソッドを書き出し

--- src/test/client/ServerHandler.java
+++ src/test/client/ServerHandler.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Date;
 
 import properties.PropertyUtil;
 import test.util.User;
@@ -36,10 +37,12 @@ public class ServerHandler {
         }
     }
 
-    // ユーザ情報を送信し、新規登録が成功したかどうかを返す
-    // 新規登録成功時には、サーバからユーザIDを取得してUserクラスに保存
-    // request -> "signUp userName password studentID"
-    // response -> "Success userID" or "failure message"
+    /*
+     * ユーザ情報を送信し、新規登録が成功したかどうかを返す
+     * 新規登録成功時には、サーバからユーザIDを取得してUserクラスに保存
+     * request -> "signUp userName password studentID"
+     * response -> "Success userID" or "failure message"
+     */
     public static boolean register() {
         String userName = User.getUserName();
         String password = User.getPassword();
@@ -89,10 +92,12 @@ public class ServerHandler {
         return isRegisterSuccess;
     }
 
-    // ログイン情報を送信し、ログインが成功したかどうかを返す
-    // ログイン成功時には、ユーザーIDと学籍番号を取得してUserクラスに保存
-    // request -> "login userName password"
-    // response -> "success userID studentID" or "failure message"
+    /*
+     * ログイン情報を送信し、ログインが成功したかどうかを返す
+     * ログイン成功時には、ユーザーIDと学籍番号を取得してUserクラスに保存
+     * request -> "login userName password"
+     * response -> "success userID studentID" or "failure message"
+     */
     public static boolean login() {
         String userName = User.getUserName();
         String password = User.getPassword();
@@ -140,6 +145,78 @@ public class ServerHandler {
         }
 
         return isLoginSuccess;
+    }
+
+    /*
+     * ユーザーIDを送信し、友達リストを取得できたかどうかを返す
+     * 友達リスト取得成功時には、友達のユーザIDとユーザー名をUserクラスに保存
+     * request -> "fetchFriend userID"
+     * response -> "success userID1:userName1 userID2:userName2 ..." or
+     * "failure message"
+     */
+    public static boolean fetchFriend() {
+        return false;
+    }
+
+    /*
+     * ユーザーIDと友達のユーザーIDを送信し、友達追加が成功したかどうかを返す
+     * 成功時には、友達のユーザーIDとユーザー名をUserクラスに保存
+     * request -> "addFriend userID friendID"
+     * response -> "success friendID:friendName" or "failure message"
+     */
+    public static boolean addFriend() {
+        return false;
+    }
+
+    /*
+     * ユーザーIDと友達のユーザーIDを送信し、友達削除が成功したかどうかを返す
+     * 成功時には、友達のユーザーIDとユーザー名をUserクラスから削除
+     * request -> "removeFriend userID friendID"
+     * response -> "success" or "failure message"
+     */
+    public static boolean removeFriend() {
+        return false;
+    }
+
+    /*
+     * ユーザーIDを送信し、予約情報を取得できたかどうかを返す
+     * 予約情報取得成功時には、予約情報をUserクラスに保存
+     * request -> "fetchReservation userID"
+     * response ->
+     * "success userID:userName,userID:userName,... cafeNum seatNum startTime endTime went"
+     */
+    public static boolean fetchReservation() {
+        return false;
+    }
+
+    /*
+     * ユーザーID、学食番号、座席番号、予約開始時間、予約終了時間を送信し、予約が成功したかどうかを返す
+     * 予約成功時には、予約情報をUserクラスに保存
+     * request ->
+     * "addReservation userID1,userID2,... cafeNum seatNum startTime endTime"
+     * response -> "success" or "failure message"
+     */
+    public static boolean addReservation() {
+        return false;
+    }
+
+    /*
+     * ユーザーIDを送信し、予約をキャンセルできたかどうかを返す
+     * キャンセル成功時には、予約情報をUserクラスから削除
+     * request -> "removeReservation userID1,userID2,..."
+     * response -> "success" or "failure message"
+     */
+    public static boolean removeReservation() {
+        return false;
+    }
+
+    /*
+     * 学食番号と予約の開始時間と終了時間を送信し、その時間に空いている座席を取得する
+     * request -> "fetchAvailableSeats startTime endTime"
+     * response -> "success seatNum1,seatNum2,seatNum3..." or "failure message"
+     */
+    public static int[] fetchAvailableSeats(int cafeNum, Date startTime, Date endTime) {
+        return new int[0];
     }
 
 }
